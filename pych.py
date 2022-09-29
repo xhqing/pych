83ea725c4e047e76d3cc12d1d5f23c5f124cb2ed
xuhuaqing <huaqingxu89@gmail.com>
2022-09-29 14:40:22 +0800

    update

---
+++ pych.py
@@ -0,0 +1,33 @@
+import re
+import pandas as pd
+from clickhouse_driver import Client
+
+class PyCH:
+    """python connect clickhouse
+    """
+
+    def __init__(self, host, dbname, username, password, port, send_receive_timeout=2000):
+        
+        self.host = host
+        self.dbname = dbname
+        self.username = username
+        self.password = password
+        self.port = port
+        self.send_receive_timeout = send_receive_timeout
+        
+        self.client = Client(
+            host=self.host,
+            database=self.dbname,
+            user=self.username,
+            password=self.password,
+            send_receive_timeout=self.send_receive_timeout,
+            port=self.port,
+        )
+
+
+    def sql2df(self, sql):
+
+        data, columns = self.client.execute(sql, columnar=True, with_column_types=True)
+        df = pd.DataFrame({re.sub(r"\W", "_", col[0]): d for d, col in zip(data, columns)})
+
+        return df
